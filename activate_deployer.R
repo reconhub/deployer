6dea79d1065065e7d992e83ec3bb469ba0bf2cd9
Thibaut Jombart <thibautjombart@gmail.com>
2017-06-27 11:12:19 +0100

    adding script to fix devel install and example

---
+++ activate_deployer.R
@@ -0,0 +1,51 @@
+## Script to activate the deployer; 'path' is the path to the RECON deployer
+## folder. It will set up the deployer as local CRAN repository, and add a
+## script for installing non-CRAN packages in the global environment.
+
+activate_deployer <- function(path = getwd()) {
+
+  ## Check that content is there
+
+  expected <- c("bin", "drat", "extra", "src")
+  present <- dir(path)
+  missing <- expected[!expected %in% present]
+  if (length(missing) > 0L) {
+    msg <- paste("Folder \"", path, "\"",
+                 "doesn't look like a RECON deployer.\n",
+                 "\nThe following elements are missing:\n",
+                 paste(missing, collapse = ", "),
+                 "\n\nPlease check the path and try again."
+                 )
+    stop(msg)
+  }
+
+
+  ## Set up RECON deployer as default package repository:
+
+  deployer_dir <- paste0("file://", normalizePath(path))
+  options(repos = deployer_dir)
+  message("Set deployer as local CRAN respository.")
+
+
+  install_devel <- function(pkg) {
+    devel_dir <- normalizePath(
+      paste(path,
+            "drat/src/contrib/", sep = "/"))
+
+    pkg_src <- dir(devel_dir, pattern = pkg, full = TRUE)
+    if (length(pkg_src) == 0L) {
+      stop("package", pkg_src, "not found")
+    }
+    install.packages(pkg_src, type = "source", repos = NULL)
+  }
+
+  assign("install_devel", install_devel, envir = .GlobalEnv)
+  message("Added 'install_devel' function to global environment.")
+  cat("\n")
+  message("Example to install CRAN package 'outbreaks':")
+  message("install.packages(\"outbreaks\")")
+  cat("\n")
+  message("Example to install devel package 'earlyR':")
+  message("install_devel(\"earlyR\")")
+
+}
