3b6e221d78dc83f06ef4fc9422f4a23c5d844cec
Zhian N. Kamvar <zkamvar@gmail.com>
2019-02-04 19:23:41 +0900

    update based on Rich's comments

--- inst/update_deployer.R
+++ inst/update_deployer.R
@@ -1,14 +1,14 @@
 #' Update a package in a deployer from the source code
 #'
 #' @param pkg the path to a package source
-#' @param repo path to a drat repo. Defaults to both the current and 'drat' repo 
+#' @param repo path to a drat repo. Defaults to the current  repo 
 #' @param internet a future parameter to update packages from internet if it is
 #'   available.
 #' @examples
 #'
 #' deploy_package("../linelist") # update the linelist package, assuming its
 #'   source lives one directory upstream of this deployer
-deploy_package <- function(pkg = NULL, repo = c(here::here(), here::here("drat")), internet = FALSE) {
+deploy_package <- function(pkg = NULL, repo = ".", internet = FALSE) {
   if (is.null(pkg)) {
     msg <- paste(
       "Please supply a path to a package source"
@@ -16,26 +16,16 @@ deploy_package <- function(pkg = NULL, repo = c(here::here(), here::here("drat")
     stop(msg)
   }
   stopifnot(file.exists(pkg))
+  stopifnot(file.exists(file.path(repo, 'src', 'contrib')))
   
   vers <- read.dcf(file.path(pkg, "DESCRIPTION"))[, "Version"]
   message("Building source package ...")
-  src <- devtools::build(pkg, path = here::here(), binary = FALSE)
-  message(sprintf("Building binary package for %s", R.version$os))
-  bin <- devtools::build(pkg, path = here::here(), binary = TRUE)
+  src <- devtools::build(pkg, path = repo, binary = FALSE)
   
 
-  for (i in repo) {
-    message(sprintf("Adding source and binary to %s", i))
-    drat::insertPackage(src, 
-                        action = "archive", 
-                        repodir = i)
-
-    drat::insertPackage(bin, 
-                        action = "archive", 
-                        repodir = i)
-  }
+  message(sprintf("Adding package and to %s", repo))
+  drat::insertPackage(src, action = "archive", repodir = repo) 
   
   unlink(src)
-  unlink(bin)
   return(NULL)
 }
