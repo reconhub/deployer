863e97ca0e25ff99dca6990eee6f8a589de3f987
Zhian N. Kamvar <zkamvar@gmail.com>
2019-05-09 15:05:48 +0100

    update add_packages

--- add_packages.R
+++ add_packages.R
@@ -7,5 +7,19 @@ imsg <- "Usage: Rscript add_packages.R [...]
 "
 args <- commandArgs(trailingOnly = TRUE)
 if (is.na(args[1])) stop(imsg)
-pkglist <- readLines("package_list.txt")
-cat(sort(c(args, pkglist)), sep = "\n", file = "package_list.txt")
+punct <- grepl("[[:punct:]]", args)
+if (any(punct)) {
+  pkgs <- paste(args[punct], collapse = "\n")
+  message(sprintf("The following packages may need to be added to package_sources.txt:\n%s", pkgs)) 
+  args <- args[!punct]
+}
+
+pkglist  <- readLines("package_list.txt")
+pkgs     <- unique(sort(c(args, pkglist)))
+new_pkgs <- setdiff(pkgs, pkglist)
+
+message(paste("new package added:", new_pkgs, collapse = "\n"))
+
+cat(pkgs, sep = "\n", file = "package_list.txt")
+
+
